84d772f6129887166adc4ed6458400efc33007fc
Grace <graceabellj@gmail.com>
2019-08-26 17:18:39 -0600

    i did a thing

---
+++ project/Assets/scripts/assignment/variables.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    string myname = "grace";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myname);
+    
+    }
+
+    // Update is called once per frame 
+        
+
+}
