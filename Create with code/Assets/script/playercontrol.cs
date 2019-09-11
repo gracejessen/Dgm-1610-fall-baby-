3b352b5522c0f6b4e1f537c6675b43ea7f78b1c9
Grace <graceabellj@gmail.com>
2019-09-11 17:05:31 -0600

    crash

---
+++ Create with code/Assets/script/playercontrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this code makes the player move.
+        transform.Translate(Vector3.forward*Time.deltaTime*69);
+    }
+}
