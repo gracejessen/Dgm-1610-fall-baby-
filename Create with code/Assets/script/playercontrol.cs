b9757bb4ca1b6f9e4e772a69e3a9bc72a6445f2c
Grace <graceabellj@gmail.com>
2019-09-18 16:49:18 -0600

    added turn controls

--- Create with code/Assets/script/playercontrol.cs
+++ Create with code/Assets/script/playercontrol.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class playercontrol : MonoBehaviour
 {
-    public float speed = 20;
+    //private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f; 
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,13 @@ public class playercontrol : MonoBehaviour
     void Update()
     {
         //this code makes the player move.
-        transform.Translate(Vector3.forward*Time.deltaTime* speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //moves car forward based on  vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rotates car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
