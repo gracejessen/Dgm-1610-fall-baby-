bff68a66e7631627d7e8cf5cdcbe3987bfb84e26
Grace <graceabellj@gmail.com>
2019-10-07 16:51:45 -0600

    added random spawning to the animals

---
+++ pizza/Assets/sctipts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] AnimalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+       
+        if (Input.GetKeyDown(KeyCode.S)) {
+            //randomly spawns animal index and position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            
+            int animalIndex = Random.Range(0, AnimalPrefabs.Length);
+             Instantiate(AnimalPrefabs[animalIndex], spawnPos, AnimalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
