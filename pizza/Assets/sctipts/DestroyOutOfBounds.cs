785cb1f335afdb2674184d8e12dd554582de0872
Grace <graceabellj@gmail.com>
2019-10-09 16:55:57 -0600

    finished pizza game

--- pizza/Assets/sctipts/DestroyOutOfBounds.cs
+++ pizza/Assets/sctipts/DestroyOutOfBounds.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroOutOfBounds : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class DestroOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.z > topBound)
+    {
+        Destroy(gameObject);
+    }else if (transform.position.z < lowerBound) 
+    {
+        Debug.Log("Game Over!");
+        Destroy(gameObject);
+    }
         
     }
 }
