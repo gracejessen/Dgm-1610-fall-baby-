d38e20b78b25951dc12a4c5585e81cb1f65b9dfb
Grace <graceabellj@gmail.com>
2019-10-23 17:07:30 -0600

    added DEATH

--- prototype 3/Assets/scritps/PlayerController.cs
+++ prototype 3/Assets/scritps/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -13,25 +14,34 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
-    { if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+    { 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        {
          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
          isOnGround = false;
-    }
+         playerAnim.SetTrigger("Jump_trig"); 
+        }
         
     }
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("ground")){
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
             isOnGround = true;
         } 
-        else if (collision.gameObject.CompareTag("obstical")){
+        else if (collision.gameObject.CompareTag("obstical"))
+        {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
